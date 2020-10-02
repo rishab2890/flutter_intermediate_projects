bf8e6ef120a9ed1a1584f8a9f8a0cd647149ddbd
rishab2890 <rishabsharma2890@gmail.com>
2020-10-02 18:46:55 +0530

    minor changes

--- flutter_location/lib/main.dart
+++ flutter_location/lib/main.dart
@@ -2,22 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_location/services/location_service.dart';
 import 'package:flutter_location/views/home_view.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_location/datamodels/user_location.dart';
 
 void main() {
   runApp(Location());
 }
 
-class Location extends StatefulWidget {
-  @override
-  _LocationState createState() => _LocationState();
-}
-
-class _LocationState extends State<Location> {
+class Location extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<UserLocation>(
         create: (context) => LocationService().locationStream,
         child: MaterialApp(title: 'Flutter Demo', home: HomeView()));
-  }
   }
 }
