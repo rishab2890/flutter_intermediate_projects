0dae2392f30d83bc8810824aec757e96f674dc59
rishab2890 <rishabsharma2890@gmail.com>
2020-10-09 18:09:37 +0530

    added slider

--- ios_style_slidebar/lib/main.dart
+++ ios_style_slidebar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() {
   runApp(App());
@@ -7,6 +8,41 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "slidebar widget",
+      home: Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Align(
+          alignment: Alignment.center,
+          child: SlideBar(),
+        ),
+      ),
+    );
+  }
+}
+
+class SlideBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.lime,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.limeAccent,
+            child: Icon(
+              Icons.fastfood,
+              color: Colors.black,
+            ),
+          ),
+          trailing: Icon(Icons.arrow_left),
+          title: Text("What do you want to eat"),
+          subtitle: Text("Choose what you are craving for"),
+        ),
+      ),
+    );
   }
 }
